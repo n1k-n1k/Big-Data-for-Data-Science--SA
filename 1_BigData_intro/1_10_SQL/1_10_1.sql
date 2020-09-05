c8be9cfbebae85043a56cbebe7ca9aa8d7c9b51a
Nik Brezginov <n1k-n1k@inbox.ru>
2020-09-05 22:46:41 +0300

    Create 1_10_1.sql

---
+++ 1_BigData_intro/1_10_SQL/1_10_1.sql
@@ -0,0 +1,7 @@
+﻿-- 1.
+-- Анализируя нашу базу данных, найдите сумму зарплат сотрудников Rick Sanchez и Morty Smith.
+-- Истинный только Rick C-137.
+
+select sum(salary) from employee
+where (name = 'Rick' and surname like 'Sanchez%137')
+   or (name = 'Morty' and surname = 'Smith');
